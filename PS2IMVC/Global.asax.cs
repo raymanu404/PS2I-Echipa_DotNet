2ebfcfb0b0a47573134d7e988476c8da14b97ab4
Mircea Dan <danaurelianmircea@yahoo.com>
2021-04-30 22:42:56 +0300

    'Frontend'

--- PS2IMVC/Global.asax.cs
+++ PS2IMVC/Global.asax.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -16,6 +19,27 @@ namespace PS2IMVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Initialise_Server();
+        }
+
+        private async void Initialise_Server()
+        {
+            TcpListener server = new TcpListener(new IPAddress(new byte[] { 0, 0, 0, 0 }), 800);
+            server.Start();
+            while(true)
+            {
+                TcpClient client = await server.AcceptTcpClientAsync();
+                RunWorker(client);
+            }
+
+        }
+
+        private async void RunWorker(TcpClient client)
+        {
+            byte[] length = new byte[1];
+            await client.GetStream().ReadAsync(length, 0, 1);
+            Debug.WriteLine(length[0].ToString());
+            client.Dispose();
         }
     }
 }
