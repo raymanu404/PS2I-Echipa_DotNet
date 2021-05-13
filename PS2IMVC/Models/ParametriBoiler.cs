b3484085aa01cc0e21bdd387960d1ca5c649f826
Mircea Dan <danaurelianmircea@yahoo.com>
2021-05-13 20:29:57 +0300

    '13.05.2021'

---
+++ PS2IMVC/Models/ParametriBoiler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PS2IMVC.Models
+{
+    public class ParametriBoiler
+    {
+        public static Boolean SystemEnable { get; set; }
+        public static Boolean PompaG1 { get; set; }
+        public static Boolean ValvaK1 { get; set; }
+        [Range(0, 100)]
+        public static int P1 { get; set; }
+        [Range(0, 100)]
+        public static int P2 { get; set; }
+        [RegularExpression(@"^[0-9]*(\.[0-9]{1,2})?$")]
+        public static decimal Capacitate { get; set; }
+        [RegularExpression(@"^[0-9]*(\.[0-9]{1,2})?$")]
+        public static decimal DebitMaxP1 { get; set; }
+        [RegularExpression(@"^[0-9]*(\.[0-9]{1,2})?$")]
+        public static decimal DebitMaxP2 { get; set; }
+        [RegularExpression(@"^[0-9]*(\.[0-9]{1,2})?$")]
+        public static decimal NivelCurent { get; set; }
+        public static int PragB1 { get; set; }
+        public static int PragB2 { get; set; }
+        public static int PragB3 { get; set; }
+        public static int PragB4 { get; set; }
+        public static int PragB5 { get; set; }
+    }
+}
