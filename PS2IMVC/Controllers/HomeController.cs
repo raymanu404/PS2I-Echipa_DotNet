91c13cf4d75477e9111209f95ee740637db96f33
Mircea Dan <danaurelianmircea@yahoo.com>
2021-05-13 20:22:24 +0300

    '13.05.2021'

--- PS2IMVC/Controllers/HomeController.cs
+++ PS2IMVC/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using PS2IMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,8 +12,34 @@ namespace PS2IMVC.Controllers
     {
         public ActionResult Index()
         {
-            //Stopwatch stopwatch;
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            if(form["button_enable"] != null)
+            {
+                if(form["button_enable"] == "false")
+                {
+                    ParametriBoiler.SystemEnable = false;
+                }
+                else if(form["button_enable"] == "true")
+                {
+                    ParametriBoiler.SystemEnable = true;
+                }
+            }
+            else if(form["P1_change"] != null)
+            {
+                ParametriBoiler.P1 = Convert.ToInt32(form["P1_change"]);
+                //Debug.WriteLine(ParametriBoiler.P1);
+            }
+            else if (form["P2_change"].ToString() != null)
+            {
+                ParametriBoiler.P2 = Convert.ToInt32(form["P2_change"]);
+                //Debug.WriteLine(ParametriBoiler.P2);
+            }
+            return Json( new { Message = "success", JsonRequestBehavior.AllowGet } );
         }
     }
 }
--- PS2IMVC/Global.asax.cs
+++ PS2IMVC/Global.asax.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using PS2IMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -15,11 +17,50 @@ namespace PS2IMVC
     {
         protected void Application_Start()
         {
+            ParametriBoiler.SystemEnable = false;
+            ParametriBoiler.PompaG1 = false;
+            ParametriBoiler.ValvaK1 = false;
+            ParametriBoiler.P1 = 0;
+            ParametriBoiler.P2 = 0;
+            ParametriBoiler.Capacitate = 10000;
+            ParametriBoiler.DebitMaxP1 = 100;
+            ParametriBoiler.DebitMaxP2 = 100;
+            ParametriBoiler.NivelCurent = 0;
+            ParametriBoiler.PragB1 = 1000;
+            ParametriBoiler.PragB2 = 2000;
+            ParametriBoiler.PragB3 = 3000;
+            ParametriBoiler.PragB4 = 4000;
+            ParametriBoiler.PragB5 = 5000;
+
+
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             Initialise_Server();
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            timer_10ms(stopwatch);
+        }
+
+        private void timer_10ms(Stopwatch stopwatch)
+        {
+            Task task = new Task(() => {
+                while(true)
+                {
+                    Debug.WriteLine(stopwatch.ElapsedTicks*1000/Stopwatch.Frequency);
+                    if (stopwatch.ElapsedTicks * 1000 / Stopwatch.Frequency >= 1000)
+                    {
+                        stopwatch.Stop();
+                        stopwatch.Reset();
+                        Debug.WriteLine("1 second passed");
+                        stopwatch.Start();
+                    }
+                }
+
+            });
+            task.Start();
         }
 
         private async void Initialise_Server()
